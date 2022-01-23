4270475bc247cb37c55d59b6b28a21ed947d2fea
TheDarkStrix <thedarkstrix@gmail.com>
2022-01-23 17:45:09 +0530

    Add object,texture,lights

--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,11 @@ import "../css/style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
 
+//Load
+
+const textureLoad = new THREE.TextureLoader();
+const texture = textureLoad.load("/textures/normalMap.png");
+
 // Debug
 const gui = new dat.GUI();
 
@@ -12,15 +17,19 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Objects
-const torus = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
+
+const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.normalMap = texture;
+material.color = new THREE.Color(0x292929);
 
 // Mesh
-const sphere = new THREE.Mesh(torus, material);
+const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
 // Lights
@@ -31,6 +40,41 @@ pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
 
+const pointLight2 = new THREE.PointLight(0xff0000, 2);
+pointLight2.position.set(-1.86, 1, -1.65);
+pointLight2.intensity = 10;
+scene.add(pointLight2);
+
+const light1 = gui.addFolder("Light 1");
+
+light1.add(pointLight2.position, "y").min(-3).max(3).step(0.1);
+light1.add(pointLight2.position, "x").min(-6).max(6).step(0.1);
+light1.add(pointLight2, "intensity").min(0).max(10).step(0.1);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1);
+scene.add(pointLightHelper);
+
+const light3 = gui.addFolder("Light 3");
+
+const pointLight3 = new THREE.PointLight(0xe1ff, 2);
+pointLight3.position.set(2.13, -3, -1.98);
+pointLight3.intensity = 6.8;
+scene.add(pointLight3);
+
+light3.add(pointLight3.position, "y").min(-3).max(3).step(0.1);
+light3.add(pointLight3.position, "x").min(-6).max(6).step(0.1);
+light3.add(pointLight3, "intensity").min(0).max(10).step(0.1);
+
+const light2Color = {
+  color: 0xff0000,
+};
+
+light3.addColor(light2Color, "color").onChange(() => {
+  pointLight3.color.set(light2Color.color);
+});
+
+const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 1);
+scene.add(pointLightHelper3);
 /**
  * Sizes
  */
@@ -77,6 +121,7 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
